747fc507914dfedaeb5b02b49cb1910e2c3c0c67
Claudio Barsante <clbmribas@gmail.com>
2021-06-14 15:50:32 -0300

    feat(Home page)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
 import Head from 'next/head';
-
+import styles from './home.module.scss';
 export default function Home() {
 	return (
 		<>
 			<Head>
-				<title>Início | ig.news</title>
+				<title>Home | ig.news</title>
 			</Head>
-			<h1>Hello Next !!!</h1>
+
+			<main className={styles.contentContainer}>
+				<section className={styles.hero}>
+					<span> 👏 Hey, welcome</span>
+					<h1>
+						News about <span>React</span> world
+					</h1>
+					<p>
+						Get access to all the publications <br />
+						<span> for $9.90 month</span>
+					</p>
+				</section>
+
+				<img src='/images/avatar.svg' alt='Girls seated on a chair coding on an laptop' />
+			</main>
 		</>
 	);
 }
