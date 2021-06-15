4c28c313cdd8e0bed9d05e410ebbe2b7824552e1
Claudio Barsante <clbmribas@gmail.com>
2021-06-15 16:43:21 -0300

    feat(add user e-mail to faunadb)

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
+import { fauna } from '../../../services/fauna';
+import { query as q } from 'faunadb';
 
 export default NextAuth({
 	providers: [
@@ -9,4 +11,19 @@ export default NextAuth({
 			scope: 'read:user', // what info do you want to have access from the user -  more about scope https://docs.github.com/pt/developers/apps/building-oauth-apps/scopes-for-oauth-apps
 		}),
 	],
+	jwt: {
+		signingKey: process.env.SIGN_IN_KEY, // -- SIGN_IN_KEY could be any value generate by you
+	},
+	callbacks: {
+		async signIn(user, account, profile) {
+			const { email } = user;
+			// -- if signIn was successful return true otherwise return false
+			try {
+				await fauna.query(q.Create(q.Collection('users'), { data: { email } }));
+				return true;
+			} catch {
+				return false;
+			}
+		},
+	},
 });
